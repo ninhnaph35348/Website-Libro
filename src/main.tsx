bda4f62a919ff36ad165f62c673380a57d591829
Haolg1812 <giahaobanh2004@gmail.com>
2025-03-18 23:53:09 +0700

    FEAT : Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/auth/store.ts"; // Import store của bạn
+
 import "./index.css";
 import "./assets/css/bootstrap.min.css";
 import "./assets/css/main.css";
@@ -13,7 +17,6 @@ import "./assets/css/all.min.css";
 
 import "./assets/js/jquery-3.7.1.min.js";
 import "./assets/js/bootstrap.bundle.min.js";
-// import "./assets/js/gsap.min.js";
 import "./assets/js/jquery.counterup.min.js";
 import "./assets/js/jquery.magnific-popup.min.js";
 import "./assets/js/jquery.meanmenu.min.js";
@@ -22,10 +25,8 @@ import "./assets/js/jquery.waypoints.js";
 import "./assets/js/main.js";
 import "./assets/js/swiper-bundle.min.js";
 import "./assets/js/viewport.jquery.js";
-// import "./assets/js/wow.min.js";
 
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import AuthorProvider from "./context/Author.tsx";
 import CategoryProvider from "./context/Category.tsx";
 import PublisherProvider from "./context/Publisher.tsx";
@@ -35,23 +36,25 @@ import AdminUserProvider from "./context/UserAdmin.tsx";
 import CustomerUserProvider from "./context/UserCustomer.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <PublisherProvider>
-      <CategoryProvider>
-        <AuthorProvider>
-          <LanguageProvider>
-            <GenreProvider>
-              <AdminUserProvider>
-                <CustomerUserProvider>
-                  <StrictMode>
-                    <App />
-                  </StrictMode>
-                </CustomerUserProvider>
-              </AdminUserProvider>
-            </GenreProvider>
-          </LanguageProvider>
-        </AuthorProvider>
-      </CategoryProvider>
-    </PublisherProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <PublisherProvider>
+        <CategoryProvider>
+          <AuthorProvider>
+            <LanguageProvider>
+              <GenreProvider>
+                <AdminUserProvider>
+                  <CustomerUserProvider>
+                    <StrictMode>
+                      <App />
+                    </StrictMode>
+                  </CustomerUserProvider>
+                </AdminUserProvider>
+              </GenreProvider>
+            </LanguageProvider>
+          </AuthorProvider>
+        </CategoryProvider>
+      </PublisherProvider>
+    </BrowserRouter>
+  </Provider>
 );

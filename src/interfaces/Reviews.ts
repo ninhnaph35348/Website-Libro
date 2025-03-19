f92899c1a8cc27a3e99240db193123a1ac2cf64b
thangcph44444 <congthangdajk@gmail.com>
2025-03-19 18:44:30 +0700

    update

--- src/interfaces/Reviews.ts
+++ src/interfaces/Reviews.ts
@@ -2,7 +2,9 @@ export interface IReviews {
     id: number;
     rating: number;
     review: string;
-    user_id: number;
-    product_id: number;
+    username: string;
+    title: string ;
     del_flg: number;
+    avatar?: string;
+    created_at:number
 }

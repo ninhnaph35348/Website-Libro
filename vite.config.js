483ba16392f7c26909fdfb94b1b186701d3d8758
ninhnaph35348 <bombimquabombim12@gmail.com>
2025-04-07 18:17:26 +0700

    fix bug, fix confilict

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "libro.local",
-    port: 5173,
-  },
+  // server: {
+  //   host: "libro.local",
+  //   port: 5173,
+  // },
 });

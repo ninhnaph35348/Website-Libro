44994d25a2072e6e5859c77e8110340a7535b03f
dongvmph36782 <dongvmph36782@fpt.edu.vn>
2025-04-24 18:52:12 +0700

    Error Checkout

--- src/services/Checkout.ts
+++ src/services/Checkout.ts
@@ -6,7 +6,12 @@ export const createCheckout = async (checkoutData: ICheckout) => {
   try {
     const { data } = await instance.post("carts/order/checkout", checkoutData);
     return data;
-  } catch (error) {
-    throw new Error("Lỗi");
+  } catch (error: any) {
+    if (error.response && error.response.data && error.response.data.message) {
+      throw new Error(error.response.data.message);
+    }
+    // Nếu không có message rõ ràng, thì fallback
+    throw new Error(error.message || "Lỗi không xác định");
   }
+
 };

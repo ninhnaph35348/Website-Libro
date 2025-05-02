c68adce3fd71ec82127495c84b52e0e80e9a4bbb
dongvmph36782 <dongvmph36782@fpt.edu.vn>
2025-05-02 08:39:14 +0700

    Fix Order

--- src/interfaces/Orders.ts
+++ src/interfaces/Orders.ts
@@ -20,6 +20,7 @@ export interface IOrder {
     status: number;
     created_at: string;
     note: string;
+    voucher_discount_type: string
     items: {
         code: string;
         cover: string;
